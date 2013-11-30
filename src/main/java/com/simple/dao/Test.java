68cb54ad68016647c93b59de0151cccda832d36b
Bowiefei <1242113705@qq.com>
2013-11-30 23:38:12 +0800

    x

---
+++ src/main/java/com/simple/dao/Test.java
@@ -0,0 +1,17 @@
+package com.simple.dao;
+
+/**
+ * @author Administrator
+ * @since 2013-11-30
+ */
+public class Test {
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+   System.out.println("hello");
+
+  }
+
+}
